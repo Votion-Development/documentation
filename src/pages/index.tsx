d74e245b4bbbc94a7baa581afad76c8f7a572d9a
Shane Caffoe <shane@scaffoe.com>
2023-12-03 05:08:37 +0000

    Current Progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,12 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 
-function HomepageHeader() {
+function HomepageHeader(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
-        <img src='/img/logo.png' />
+        <img src='/img/logo.svg' />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
@@ -36,7 +36,7 @@ function HomepageHeader() {
   );
 }
 
-export default function Home() {
+export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
